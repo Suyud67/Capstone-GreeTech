57bc69d22052691a0a06c0988711f4763550711d
Mahathir Mohamad <83440944+Mahathir-Mohamad@users.noreply.github.com>
2022-11-26 23:35:17 +0800

    update AboutTech.jsx
    update component about pada aplikasi

--- src/pages/AboutTech.jsx
+++ src/pages/AboutTech.jsx
@@ -3,21 +3,6 @@ import gambar from "../img/imageAbout.png";
 
 export default function AboutTech() {
   return (
-    <div className="py-36 px-10 lg:flex lg:items-center lg:justify-between">
-      <div className="text lg:items-center">
-        <h1 className="text-xl font-bold">About US</h1>
-        <p className="py-2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere porro
-          veritatis, aut eaque dolore tempora vitae maiores nihil sunt nostrum?
-          Delectus ad officiis quo quae quam facere culpa quod nobis?
-        </p>
-        <p className="mb-2">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-          velit eligendi optio, facere tempora voluptate id ducimus vitae vel
-          quisquam.
-        </p>
-      </div>
-      <img src={gambar} alt="" />
-    </div>
+    <About />
   );
 }
